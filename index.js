0b151ed239c25b9e7ede4bf609c6c35912f4d54e
StefanoMazzuka <stefano.mazzuka@gmail.com>
2018-12-25 16:32:40 +0100

    Finished

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ $(function () {
         "unicorn.png"
     ]
 
+    let pointsManagement = [];
     let points;
     let clicks;
     let firstCard = {
@@ -34,6 +35,8 @@ $(function () {
         alt: ''
     };
     let waiting;
+    let bestClicks;
+    let endGame;
 
     function shuffle(arr) {
         for (let i = arr.length - 1; i > 0; i--) {
@@ -68,6 +71,8 @@ $(function () {
             $('#guessedCards').empty();
             points = 0;
             clicks = 0;
+            bestClicks = 0;
+            endGame = 0;
             waiting = false;
 
             $('#clicks').text(`${clicks} clicks`);
@@ -94,9 +99,19 @@ $(function () {
                 $('#board').append(`<div class="row" id="row${row}">`);
                 for (let j = 0; j < numColumns; j++) {
                     $(`#row${row}`).append($(`<div class="cardContainer"><img id="${i}" alt="${currentCards[i]}" class="card" src="./img/question_mark.png"/></div>`));
+                    let card = {
+                        id: i,
+                        alt: currentCards[i],
+                        flipped: false
+                    };
+                    pointsManagement.push(card);
                     if (j != numColumns - 1) i++;
                 }
                 row++;
+            }
+
+            for (let i = 0; i < pointsManagement.length; i++) {
+                console.log("id " + pointsManagement[i].id + " alt " + pointsManagement[i].alt + " flipped " + pointsManagement[i].flipped);
             }
         }
     });
@@ -104,9 +119,18 @@ $(function () {
     $('body').on('click', 'img', function () {
         if (!waiting && $(this).prop('src').endsWith("question_mark.png")) {
             clicks++;
+            bestClicks++;
             $('#clicks').text(`${clicks} clicks`);
             $(this).prop('src', `./img/` + allCards[$(this).prop('alt')]);
 
+            if (!pointsManagement[$(this).prop('id')].flipped)
+                pointsManagement[$(this).prop('id')].flipped = true;
+            // BORRAR
+            console.clear();
+            for (let i = 0; i < pointsManagement.length; i++) {
+                console.log("id " + pointsManagement[i].id + " alt " + pointsManagement[i].alt + " flipped " + pointsManagement[i].flipped);
+            }
+            // BORRAR
             if (clicks % 2 != 0) {
                 firstCard.id = $(this).prop('id');
                 firstCard.alt = $(this).prop('alt');
@@ -116,7 +140,11 @@ $(function () {
                 secondCard.alt = $(this).prop('alt');
 
                 setTimeout(function () {
-                    if (firstCard.alt === secondCard.alt) {
+                    if (firstCard.alt === secondCard.alt) {                        
+                        endGame++;
+                        if (endGame == pointsManagement.length / 2)
+                        $('#bestClicks').text(`best range ${bestClicks} clicks`);
+                        
                         points += 20;
                         $('#points').text(`${points} points`);
                         $(`#${firstCard.id}`).attr('class', 'cardHidden');
@@ -124,14 +152,24 @@ $(function () {
                         $('#guessedCards').append($(`<img src="./img/${allCards[firstCard.alt]}" height="42" width="42"/>`));
                     } else {
                         if (points > 0) {
-                            points--;
+                            for (let i = 0; i < pointsManagement.length; i++) {
+                                if (pointsManagement[i].id != firstCard.id) {
+                                    if (pointsManagement[i].alt == firstCard.alt) {
+                                        if (pointsManagement[i].flipped == true) {
+                                            points--;
+                                            bestClicks--;
+                                        }
+                                    }
+                                }
+                            }
+
                             $('#points').text(`${points} points`);
                         }
                         $(`#${firstCard.id}`).prop('src', './img/question_mark.png');
                         $(`#${secondCard.id}`).prop('src', './img/question_mark.png');
                     }
                     waiting = false;
-                }, 1000);
+                }, 500);
 
             }
         }
